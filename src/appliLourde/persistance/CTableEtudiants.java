9386877f5813fd57381be20bd84344b36366a5b2
Antoine Liégard <liegard.antoine35@gmail.com>
2018-04-19 17:34:31 +0200

    reworked CEtudiant mettreAJourEtudiant, now handle NULL values

--- src/appliLourde/persistance/CTableEtudiants.java
+++ src/appliLourde/persistance/CTableEtudiants.java
@@ -14,7 +14,6 @@ import java.util.GregorianCalendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.sql.PreparedStatement;
-import java.sql.Types;
 
 public class CTableEtudiants {
 
@@ -144,12 +143,15 @@ public class CTableEtudiants {
     }
 
     int insererEtudiant(CEtudiant etudiant) {
-        int res = -1;
+        int res = 0;
         if (bdd.connecter() == true) {
             String dateNaissance = formaterDate(etudiant.getDateNaissance());
             String req = "INSERT INTO `tableetudiants` (`idSessionFormation`, "
-                    + "`nom`, `prenom`, `dateNaissance`,`numeroSS`, `commune`, `codePostal`, `numeroVoie`, "
-                    + "`typeVoie`, `nomVoie`, `mail`, `telephone1`, `telephone2`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);";
+                    + "`nom`, `prenom`, `dateNaissance`,`numeroSS`, `commune`, "
+                    + ""
+                    + "`codePostal`, `numeroVoie`, "
+                    + "`typeVoie`, `nomVoie`, `mail`, `telephone1`, `telephone2`) "
+                    + "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);";
 
             try {
                 
@@ -184,6 +186,7 @@ public class CTableEtudiants {
                 pstmt.execute();
             } catch (SQLException ex) {
                 Logger.getLogger(CBDD.class.getName()).log(Level.SEVERE, null, ex);
+                res=1;
             }
 
             bdd.deconnecter();
@@ -194,24 +197,50 @@ public class CTableEtudiants {
     }
 
     int mettreAJourEtudiant(CEtudiant etudiant) {
-        int res = -1;
+        int res = 0;
         if (bdd.connecter() == true) {
             String dateNaissance = formaterDate(etudiant.getDateNaissance());
-            String req = "UPDATE tableetudiants"
-                    + "SET `IdSessionFormation` = '" + etudiant.getIdSessionFormation()
-                    + "SET `nom` = '" + etudiant.getNom()
-                    + "SET `dateNaissance` = '" + etudiant.getDateNaissance()
-                    + "SET `numeroSS` = '" + etudiant.getNumeroSS()
-                    + "SET `commune` = '" + etudiant.getCommune()
-                    + "SET `codePostal` = '" + etudiant.getCodePostal()
-                    + "SET `numeroVoie` = '" + etudiant.getNumeroVoie()
-                    + "SET `typeVoie` = '" + etudiant.getTypeVoie()
-                    + "SET `nomVoie` = '" + etudiant.getNomVoie()
-                    + "SET `mail` = '" + etudiant.getMail()
-                    + "SET `telephone1` = '" + etudiant.getTelephone1()
-                    + "SET `telephone2` = '" + etudiant.getTelephone2()
-                    + "' WHERE `tableetudiants`.`id` =" + etudiant.getIdEtudiant() + ";";
-            res = bdd.executerRequeteUpdate(req);
+            
+            String req = "UPDATE tableetudiants "
+                    + "SET `IdSessionFormation` = ? , `nom` = ? , `prenom` = ? , `dateNaissance` = ? , `numeroSS` = ? , `commune` = ? , `codePostal` = ? , `numeroVoie` = ? , `typeVoie` = ? , `nomVoie` = ? , `mail` = ? , `telephone1` = ? , `telephone2` = ? WHERE `tableetudiants`.`idEtudiant` =" + etudiant.getIdEtudiant() + ";";
+            try {
+                
+                PreparedStatement pstmt = bdd.conn.prepareStatement(req);
+                if (etudiant.getIdSessionFormation() <= 0) {
+                    pstmt.setNull(1, java.sql.Types.INTEGER);
+                } else {
+                    pstmt.setInt(1, etudiant.getIdSessionFormation());
+                }
+                pstmt.setString(2, etudiant.getNom());
+                pstmt.setString(3, etudiant.getPrenom());
+                pstmt.setString(4, dateNaissance);
+                pstmt.setString(5, etudiant.getNumeroSS());
+                pstmt.setString(6, etudiant.getCommune());
+                pstmt.setString(7, etudiant.getCodePostal());
+                
+                if ("".equals(etudiant.getNumeroVoie())) {
+                    pstmt.setNull(8, java.sql.Types.VARCHAR);
+                } else {
+                    pstmt.setString(8, etudiant.getNumeroVoie());
+                }
+                pstmt.setString(9, etudiant.getTypeVoie());
+                pstmt.setString(10, etudiant.getNomVoie());
+                pstmt.setString(11, etudiant.getMail());
+                pstmt.setString(12, etudiant.getTelephone1());
+
+                if ("".equals(etudiant.getTelephone2())) {
+                    pstmt.setNull(13, java.sql.Types.VARCHAR);
+                } else {
+                    pstmt.setString(13, etudiant.getTelephone2());
+                }
+                System.out.println(pstmt);
+                pstmt.execute();
+                res=1;
+            } catch (SQLException ex) {
+                Logger.getLogger(CBDD.class.getName()).log(Level.SEVERE, null, ex);
+                res = 0;
+            }
+            
             bdd.deconnecter();
         } else {
             System.out.println("Connexion KO");
@@ -264,24 +293,41 @@ public class CTableEtudiants {
         CTableEtudiants tableEtudiants = new CTableEtudiants();
         tableEtudiants.setBdd(bdd);  // Création de l'association simple entre TableEtudiants et CBDD
 
-        for (int i = 0; i < 5; i++) {
-            int rand = (int) (Math.random() * 100);
-            tableEtudiants.insererEtudiant(new CEtudiant(
-                    -1,
-                    -1,
-                    "Nom" + rand,
-                    "Prénom" + rand,
-                    new GregorianCalendar(),
-                    "196114634948493",
-                    "Rennes",
-                    "35000",
-                    "1",
-                    "Rue",
-                    "Bô jeu",
-                    "test.test@gmail.com",
-                    "0102030405",
-                    "0203040506"));
-        }
+//        for (int i = 0; i < 5; i++) {
+//            int rand = (int) (Math.random() * 100);
+//            tableEtudiants.insererEtudiant(new CEtudiant(
+//                    -1,
+//                    -1,
+//                    "Nom" + rand,
+//                    "Prénom" + rand,
+//                    new GregorianCalendar(),
+//                    "196114634948493",
+//                    "Rennes",
+//                    "35000",
+//                    "1",
+//                    "Rue",
+//                    "Bô jeu",
+//                    "test.test@gmail.com",
+//                    "0102030405",
+//                    "0203040506"));
+//        }
+        
+//        CEtudiant etudiant = new CEtudiant(
+//                    49,
+//                    -1,
+//                    "Chevalier",
+//                    "Alexis",
+//                    new GregorianCalendar(),
+//                    "196114634948493",
+//                    "Rennes",
+//                    "35000",
+//                    "1",
+//                    "Rue",
+//                    "Bô jeu",
+//                    "test.test@gmail.com",
+//                    "0102030405",
+//                    "0203040506");
+//        tableEtudiants.mettreAJourEtudiant(etudiant);
 
         CListeEtudiants listeEtudiants = tableEtudiants.lireEtudiants();
         listeEtudiants.toString();
